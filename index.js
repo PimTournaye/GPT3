3d9586bfab2919263d209cd131f83e86d6298e73
Pim Tournaye <pim.tournaye@student.ehb.be>
2021-06-01 00:19:55 +0200

    map section added

--- index.js
+++ index.js
@@ -6,10 +6,27 @@ const dotenv = require('dotenv').config({
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const openai = new OpenAI(OPENAI_API_KEY);
+const coordsPrestatyn = [53.335400, -3.406320];
+const coordsLlandudno = [53.326542, -3.834460];
+const coordsLlanfairfechan = [53.253899, -3.978390];
+
+
+// MAP
+
+let map = L.map('map').setView(coords, 13);
+
+let marker1 = L.marker(coordsPrestatyn).addTo(map);
+marker1.bindPopup("<b>Here is our start</b><br> What stories will you hear today.");
+
+let marker2 = L.marker(coordsLlandudno).addTo(map);
+marker2.bindPopup("<b>This is the midway point.</b><br> Exciting!");
+
+let marker3 = L.marker(coordsLlanfairfechan).addTo(map);
+marker3.bindPopup("<b>This is the end!</b><br> Time to go back home.");
+
 
 //const prompt = "Small intriguing stories about places you pass by on bike rides, like the Wales Coast Path. The first location is Prestatyn. The second location is Llandudno. The final location is Llanfairfechan. 1. Our story begins at Prestatyn, a seaside town with a dark, historical secret "
-
-
+const prompt= "Tell me a curious story about the coast of Prestatyn."
 
 let compeletePrompt = async (prompt) => {
     const gptResponse = await openai.complete({
