7180ef260808f36c41594f643491b0c9cc06d465
MiguelBilliet1995 <miguel.billiet@student.ehb.be>
2021-06-01 00:38:19 +0200

    Adding node

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Express
+
+const express = require('express')
+const app = express()
+
+// Openai API
+
 const OpenAI = require('openai-api');
 const dotenv = require('dotenv').config({
     path: __dirname + '/.env'
@@ -10,8 +17,16 @@ const openai = new OpenAI(OPENAI_API_KEY);
 //const prompt = "Small intriguing stories about places you pass by on bike rides, like the Wales Coast Path. The first location is Prestatyn. The second location is Llandudno. The final location is Llanfairfechan. 1. Our story begins at Prestatyn, a seaside town with a dark, historical secret "
 
 
+let compeletePrompt = () => {
+    return ({
+    text: 'View Full Version : Blu-ray on mac mini via HDMI?',
+    index: 0,
+    logprobs: null,
+    finish_reason: 'stop'
+  });
+}
 
-let compeletePrompt = async (prompt) => {
+/*let compeletePrompt = async (prompt) => {
     const gptResponse = await openai.complete({
         engine: 'davinci',
         prompt: prompt,
@@ -25,7 +40,23 @@ let compeletePrompt = async (prompt) => {
         stream: false,
         stop: ['\n', "testing"]
     });
-    console.log(gptResponse.data.choices);
-};
 
-compeletePrompt();
+    //console.log(gptResponse.data.choices);
+
+    return gptResponse.data.choices;
+};*/
+
+
+
+// Routes
+
+app.use(express.static('frontend'));
+
+app.post('/generate', async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(await compeletePrompt()));
+});
+
+
+
+app.listen(3000)
